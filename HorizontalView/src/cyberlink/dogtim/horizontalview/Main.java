10150f08226d3ac98457be676239e64939ce8d17
dogtim <fatfattim@gmail.com>
2013-10-23 14:43:03 +0800

    Make duration track expendable
    1. Set minSdkVersion to 11 because setMotionEventSplittingEnabled API need it.
    2. Practice customize layout extends from relative layout

--- HorizontalView/src/cyberlink/dogtim/horizontalview/Main.java
+++ HorizontalView/src/cyberlink/dogtim/horizontalview/Main.java
@@ -2,46 +2,66 @@ package cyberlink.dogtim.horizontalview;
 
 import java.util.ArrayList;
 
+import cyberlink.dogtim.horizontalview.widgets.TimelineRelativeLayout;
+
 import android.os.Bundle;
 import android.app.Activity;
 import android.graphics.Bitmap;
 import android.util.Log;
 import android.view.Gravity;
 import android.view.View;
+import android.view.ViewGroup;
 import android.view.ViewGroup.LayoutParams;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 
+
 public class Main extends Activity {
     private static final String TAG = Main.class.getSimpleName();
-    
-    private LeakTest leak = null;
-    class LeakTest {
-        void trackLeak(){
-            Log.d(TAG,"Track Leak ! ! !");
-        }
-    }
-    LinearLayout imageLayout;
-    LinearLayout timelineLayout;
+
+    private LinearLayout imageLayout;
+    private LinearLayout timelineLayout;
+    private TimelineRelativeLayout mTimelineLayout;
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         imageLayout = (LinearLayout) findViewById(R.id.imageLayout);
         timelineLayout = (LinearLayout) findViewById(R.id.timelineHorizontalScrollView);
-        if(leak == null){
-            leak = new LeakTest();
-        }
-        
+        mTimelineLayout = (TimelineRelativeLayout)findViewById(R.id.timeline);
+
         MediaStoreHelper mediaStoreHelper = new MediaStoreHelper(this.getApplicationContext()); 
         ArrayList<String> Files_string = mediaStoreHelper.getImages();
         for (String file : Files_string) {
             imageLayout.addView(insertPhoto(file));
             timelineLayout.addView(insertPhoto(file));
         }
+
+         
+        measureTimeLineWidth();
     }
 
-    View insertPhoto(String path) {
+    /**
+     *   This is a workaround to get measure spec because <p>
+     *   we can not get correct value in onCreate function. <p>
+     *   @see <a href="http://www.eoeandroid.com/thread-240677-1-1.html">getWidth() == 0</a>
+     */
+    private void measureTimeLineWidth(){
+        
+        int w = View.MeasureSpec.makeMeasureSpec(0,View.MeasureSpec.UNSPECIFIED);  
+        int h = View.MeasureSpec.makeMeasureSpec(0,View.MeasureSpec.UNSPECIFIED);  
+        timelineLayout.measure(w, h); 
+        final int childrenCount = mTimelineLayout.getChildCount();
+        for (int i = 0; i < childrenCount; i++) {
+            final View child = mTimelineLayout.getChildAt(i);
+            final ViewGroup.LayoutParams lp = child.getLayoutParams();
+
+            lp.width = timelineLayout.getMeasuredWidth();
+            child.setLayoutParams(lp);
+        }
+    }
+    private View insertPhoto(String path) {
         Bitmap bm = BitmapHelper.decodeSampledBitmapFromUri(path, 40, 40);
 
         LinearLayout layout = new LinearLayout(getApplicationContext());
