1d526088949833a8d4109c1772f27f0ab8c98434
dogtim <fatfattim@gmail.com>
2013-10-23 15:48:46 +0800

    Make PlayheadView not block the preview Graphic function

--- HorizontalView/src/cyberlink/dogtim/horizontalview/widgets/PlayheadView.java
+++ HorizontalView/src/cyberlink/dogtim/horizontalview/widgets/PlayheadView.java
@@ -19,63 +19,70 @@ import android.view.WindowManager;
 public class PlayheadView extends View {
     private static final String TAG = PlayheadView.class.getSimpleName();
     // Instance variables
-    private final Paint mLinePaint;
-    private final Paint mTextPaint;
-    private final int mTicksHeight;
+    private Paint mLinePaint;
+    private Paint mTextPaint;
+    private int mTicksHeight;
     // Timeline text size.
-    private final float mTimeTextSize;
-    private final int mScreenWidth;
-    private final ScrollViewListener mScrollListener;
+    private float mTimeTextSize;
+    private int mScreenWidth;
+    private ScrollViewListener mScrollListener;
     private int mScrollX;
     //private VideoEditorProject mProject;
 
     public PlayheadView(Context context, AttributeSet attrs, int defStyle) {
         super(context, attrs, defStyle);
+        if (!isInEditMode()) {
+            final Resources resources = context.getResources();
 
-        final Resources resources = context.getResources();
+            // Prepare the Paint used to draw the tick marks
+            mLinePaint = new Paint();
+            mLinePaint
+                    .setColor(resources.getColor(R.color.playhead_tick_color));
+            mLinePaint.setStrokeWidth(2);
+            mLinePaint.setStyle(Paint.Style.STROKE);
 
-        // Prepare the Paint used to draw the tick marks
-        mLinePaint = new Paint();
-        mLinePaint.setColor(resources.getColor(R.color.playhead_tick_color));
-        mLinePaint.setStrokeWidth(2);
-        mLinePaint.setStyle(Paint.Style.STROKE);
+            // Prepare the Paint used to draw the text
+            mTextPaint = new Paint();
+            mTextPaint.setAntiAlias(true);
+            mTextPaint
+                    .setColor(resources.getColor(R.color.playhead_tick_color));
+            mTimeTextSize = resources
+                    .getDimension(R.dimen.playhead_layout_text_size);
+            mTextPaint.setTextSize(mTimeTextSize);
 
-        // Prepare the Paint used to draw the text
-        mTextPaint = new Paint();
-        mTextPaint.setAntiAlias(true);
-        mTextPaint.setColor(resources.getColor(R.color.playhead_tick_color));
-        mTimeTextSize = resources.getDimension(R.dimen.playhead_layout_text_size);
-        mTextPaint.setTextSize(mTimeTextSize);
+            // The ticks height
+            mTicksHeight = (int) resources
+                    .getDimension(R.dimen.playhead_tick_height);
 
-        // The ticks height
-        mTicksHeight = (int)resources.getDimension(R.dimen.playhead_tick_height);
+            // Get the screen width
+            final Display display = ((WindowManager) context
+                    .getSystemService(Context.WINDOW_SERVICE))
+                    .getDefaultDisplay();
+            final DisplayMetrics metrics = new DisplayMetrics();
+            display.getMetrics(metrics);
+            mScreenWidth = metrics.widthPixels;
+            // Listen to scroll events and repaint this view as needed
+            mScrollListener = new ScrollViewListener() {
 
-        // Get the screen width
-        final Display display = ((WindowManager)context.getSystemService(
-                Context.WINDOW_SERVICE)).getDefaultDisplay();
-        final DisplayMetrics metrics = new DisplayMetrics();
-        display.getMetrics(metrics);
-        mScreenWidth = metrics.widthPixels;
+                @Override
+                public void onScrollBegin(View view, int scrollX, int scrollY, boolean appScroll) {
+                }
 
-        // Listen to scroll events and repaint this view as needed
-        mScrollListener = new ScrollViewListener() {
+                @Override
+                public void onScrollProgress(View view, int scrollX, int scrollY, boolean appScroll) {
+                    mScrollX = scrollX;
+                    invalidate();
+                }
 
-            @Override
-            public void onScrollBegin(View view, int scrollX, int scrollY, boolean appScroll) {
-            }
+                @Override
+                public void onScrollEnd(View view, int scrollX, int scrollY, boolean appScroll) {
+                    mScrollX = scrollX;
+                    invalidate();
+                }
+            };
+        }
 
-            @Override
-            public void onScrollProgress(View view, int scrollX, int scrollY, boolean appScroll) {
-                mScrollX = scrollX;
-                invalidate();
-            }
 
-            @Override
-            public void onScrollEnd(View view, int scrollX, int scrollY, boolean appScroll) {
-                mScrollX = scrollX;
-                invalidate();
-            }
-        };
     }
 
     public PlayheadView(Context context, AttributeSet attrs) {
@@ -88,10 +95,12 @@ public class PlayheadView extends View {
 
     @Override
     protected void onAttachedToWindow() {
-        final TimelineHorizontalScrollView scrollView =
-            (TimelineHorizontalScrollView)((View)getParent()).getParent();
-        mScrollX = scrollView.getScrollX();
-        scrollView.addScrollListener(mScrollListener);
+        if (!isInEditMode()) {
+            final TimelineHorizontalScrollView scrollView = (TimelineHorizontalScrollView) ((View) getParent())
+                    .getParent();
+            mScrollX = scrollView.getScrollX();
+            scrollView.addScrollListener(mScrollListener);
+        }
     }
 
     @Override
@@ -115,26 +124,30 @@ public class PlayheadView extends View {
          * hard code to define durationMs
          * this variable could determine how many column we would split on duratio track
          * */
-        final long durationMs = 100000;
+        if (!isInEditMode()) {
+            final long durationMs = 100000;
 
-        final int y = (int) -mTextPaint.getFontMetrics().top;
+            final int y = (int) -mTextPaint.getFontMetrics().top;
 
-        final int width = getWidth() - mScreenWidth;
+            final int width = getWidth() - mScreenWidth;
 
-        final long tickMs = 10000;
+            final long tickMs = 10000;
 
-        final float spacing = ((float) (width * tickMs) / (float) durationMs);
-        final float startX = 50;
-        float startMs = 0;
-        
-        final Context context = getContext();
-        final float endX = mScrollX + mScreenWidth;
+            final float spacing = ((float) (width * tickMs) / (float) durationMs);
+            final float startX = 50;
+            float startMs = 0;
 
-        for (float i = startX; i <= endX; i += spacing, startMs += tickMs) {
-            final String timeText = StringUtils.getSimpleTimestampAsString(context, (long) startMs);
-            final int x = (int) (i - mTextPaint.measureText(timeText) / 2);
-            canvas.drawText(timeText, x, y, mTextPaint);
-            canvas.drawLine(i, mTimeTextSize, i, mTimeTextSize + mTicksHeight, mLinePaint);
+            final Context context = getContext();
+            final float endX = mScrollX + mScreenWidth;
+
+            for (float i = startX; i <= endX; i += spacing, startMs += tickMs) {
+                final String timeText = StringUtils.getSimpleTimestampAsString(
+                        context, (long) startMs);
+                final int x = (int) (i - mTextPaint.measureText(timeText) / 2);
+                canvas.drawText(timeText, x, y, mTextPaint);
+                canvas.drawLine(i, mTimeTextSize, i, mTimeTextSize
+                        + mTicksHeight, mLinePaint);
+            }
         }
     }
 }
