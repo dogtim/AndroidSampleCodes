2a1ef0e7df3c4530e6e321c8ad373538c32bf2b8
dogtim <fatfattim@gmail.com>
2013-10-24 16:09:47 +0800

    Add animation when insert item

--- HorizontalView/src/cyberlink/dogtim/horizontalview/Main.java
+++ HorizontalView/src/cyberlink/dogtim/horizontalview/Main.java
@@ -5,6 +5,10 @@ import java.util.ArrayList;
 import cyberlink.dogtim.horizontalview.widgets.TimelineRelativeLayout;
 
 import android.os.Bundle;
+import android.animation.Animator;
+import android.animation.AnimatorListenerAdapter;
+import android.animation.LayoutTransition;
+import android.animation.ObjectAnimator;
 import android.app.Activity;
 import android.content.ClipData;
 import android.content.Context;
@@ -15,6 +19,8 @@ import android.view.Gravity;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.ViewGroup.LayoutParams;
+import android.view.animation.Animation;
+import android.view.animation.TranslateAnimation;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 
@@ -22,8 +28,8 @@ import android.widget.LinearLayout;
 public class Main extends Activity {
     private static final String TAG = Main.class.getSimpleName();
 
-    private LinearLayout imageLayout;
-    private LinearLayout timelineLayout;
+    private LinearLayout mMaterialLayout;
+    private LinearLayout mPhotoTrackLayout;
     private TimelineRelativeLayout mTimelineLayout;
     
     private static enum ItemType {
@@ -52,22 +58,35 @@ public class Main extends Activity {
             final int action = event.getAction();
             Item item = selectItem;
             Log.d(TAG,"onDrag item: "+item.type.getName());
+            Item target = (Item) v.getTag();
+            if (target == null)
+                return true;
             switch (action) {
                 case DragEvent.ACTION_DRAG_STARTED:
                     Log.d(TAG,"onDrag ACTION_DRAG_STARTED");
                     break;
                 
                 case DragEvent.ACTION_DRAG_ENTERED:
+                    
+                    if(target.type == ItemType.EditingItem){
+
+                        /*Animation am = new TranslateAnimation( 0, 100, 0, 0);
+                        am.setDuration(2500);
+                        mTimelineLayout.setAnimation(am);
+                        am.startNow();*/
+                        /*v.setPadding(200, 0, 0, 0);
+                        mTimelineLayout.invalidate();*/
+                        //v.setVisibility(View.INVISIBLE);
+                    }
                     Log.d(TAG,"onDrag ACTION_DRAG_ENTERED");
                     break;
                 case DragEvent.ACTION_DRAG_LOCATION:
                     Log.d(TAG,"onDrag ACTION_DRAG_LOCATION");
                     break;
                 case DragEvent.ACTION_DROP:
-                    Item target = (Item) v.getTag();
                     if(target.type == ItemType.EditingItem){
-                        Log.d(TAG,"dogtim ba ba");
-                        timelineLayout.addView(insertPhotoToTimeLine(item.path));
+                        int id = mPhotoTrackLayout.indexOfChild(v);
+                        mPhotoTrackLayout.addView(insertPhotoToTimeLine(item.path),id);
                         measureTimeLineWidth();
                         mTimelineLayout.invalidate();
                     }
@@ -93,26 +112,45 @@ public class Main extends Activity {
             return true;
         }
     };
-    
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        imageLayout = (LinearLayout) findViewById(R.id.imageLayout);
-        timelineLayout = (LinearLayout) findViewById(R.id.timelineHorizontalScrollView);
+        mMaterialLayout = (LinearLayout) findViewById(R.id.imageLayout);
+        mPhotoTrackLayout = (LinearLayout) findViewById(R.id.photo_track);
         mTimelineLayout = (TimelineRelativeLayout)findViewById(R.id.timeline);
-
-        MediaStoreHelper mediaStoreHelper = new MediaStoreHelper(this.getApplicationContext()); 
-        ArrayList<String> Files_string = mediaStoreHelper.getImages();
-        for (String file : Files_string) {
-            imageLayout.addView(insertPhotoToMaterial(file));
-            timelineLayout.addView(insertPhotoToTimeLine(file));
-        }
-
-         
+        //mPhotoTrackLayout.setClipChildren(false);
+        //mTimelineLayout.setClipChildren(false);
+        initAnimation();
+        prepareMaterial();
         measureTimeLineWidth();
     }
-
+    
+    private void initAnimation(){
+        final LayoutTransition transitioner = new LayoutTransition();
+        mPhotoTrackLayout.setLayoutTransition(transitioner);
+        Animator customAppearingAnim;
+        // Adding
+        customAppearingAnim = ObjectAnimator.ofFloat(null, "rotationY", 90f, 0f).
+                setDuration(transitioner.getDuration(LayoutTransition.APPEARING));
+        customAppearingAnim.addListener(new AnimatorListenerAdapter() {
+            public void onAnimationEnd(Animator anim) {
+                View view = (View) ((ObjectAnimator) anim).getTarget();
+                view.setRotationY(0f);
+            }
+        });
+        transitioner.setAnimator(LayoutTransition.APPEARING, customAppearingAnim);
+    }
+    
+    private void prepareMaterial(){
+        MediaStoreHelper mediaStoreHelper = new MediaStoreHelper(getApplicationContext()); 
+        ArrayList<String> Files_string = mediaStoreHelper.getImages();
+        for (String file : Files_string) {
+            mMaterialLayout.addView(insertPhotoToMaterial(file));
+            mPhotoTrackLayout.addView(insertPhotoToTimeLine(file));
+        }
+    }
     /**
      *   This is a workaround to get measure spec because <p>
      *   we can not get correct value in onCreate function. <p>
@@ -122,13 +160,13 @@ public class Main extends Activity {
         
         int w = View.MeasureSpec.makeMeasureSpec(0,View.MeasureSpec.UNSPECIFIED);  
         int h = View.MeasureSpec.makeMeasureSpec(0,View.MeasureSpec.UNSPECIFIED);  
-        timelineLayout.measure(w, h); 
+        mPhotoTrackLayout.measure(w, h); 
         final int childrenCount = mTimelineLayout.getChildCount();
         for (int i = 0; i < childrenCount; i++) {
             final View child = mTimelineLayout.getChildAt(i);
             final ViewGroup.LayoutParams lp = child.getLayoutParams();
 
-            lp.width = timelineLayout.getMeasuredWidth();
+            lp.width = mPhotoTrackLayout.getMeasuredWidth();
             child.setLayoutParams(lp);
         }
     }
@@ -163,11 +201,8 @@ public class Main extends Activity {
         layout.addView(imageView);
         return layout;
     }
-    private View insertPhotoToTimeLine(String path) {
-        LinearLayout layout = new LinearLayout(getApplicationContext());
-        layout.setLayoutParams(new LayoutParams(250, 250));
-        layout.setGravity(Gravity.CENTER);
 
+    private View insertPhotoToTimeLine(String path) {
         ImageView imageView = createImageView(path);
         /* TODO 
          * maybe we could store object in setTag to split different object
@@ -177,7 +212,7 @@ public class Main extends Activity {
         item.type = ItemType.EditingItem;
         item.path = path;
         imageView.setTag(item);
-        layout.addView(imageView);
-        return layout;
+        return imageView;
+
     }
 }
