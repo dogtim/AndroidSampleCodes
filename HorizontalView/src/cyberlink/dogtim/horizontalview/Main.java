236c078e7f9c5c84bd0a6865ec6938c881fc399a
Hsiao-Ting Chen <fatfattim@gmail.com>
2013-11-15 22:11:19 -0800

    Add internet_permission

--- HorizontalView/src/cyberlink/dogtim/horizontalview/Main.java
+++ HorizontalView/src/cyberlink/dogtim/horizontalview/Main.java
@@ -5,6 +5,7 @@ import java.util.ArrayList;
 import cyberlink.dogtim.horizontalview.listener.EditingOnDragListener;
 import cyberlink.dogtim.horizontalview.util.MaterialShadowBuilder;
 import cyberlink.dogtim.horizontalview.util.UIUtil;
+import cyberlink.dogtim.horizontalview.util.ViewServer;
 import cyberlink.dogtim.horizontalview.widgets.PlayheadView;
 import cyberlink.dogtim.horizontalview.widgets.TimelineHorizontalScrollView;
 import cyberlink.dogtim.horizontalview.widgets.TimelineRelativeLayout;
@@ -40,7 +41,7 @@ public class Main extends Activity {
     private Project mProject;
     private Activity mActivity = this;
     private EditingOnDragListener mDragListener;
-    private boolean DEBUG_HV = false;
+    private boolean DEBUG_HV = true;
     private View.OnClickListener mOnMediaItemClickListener = new View.OnClickListener() {
         @Override
         public void onClick(View v) {
@@ -133,7 +134,7 @@ public class Main extends Activity {
         setTimeLineEditingItem();
         setPhotoMaterial();
         measureTimeLineWidth();
-        if (DEBUG_HV) cyberlink.dogtim.horizontalview.util.ViewServer.get(this).addWindow(this);
+        if (DEBUG_HV) ViewServer.get(this).addWindow(this);
     }
     private void setScrollController(){
         ImageView leftScrollController = (ImageView)findViewById(R.id.left_scroll_controller);
