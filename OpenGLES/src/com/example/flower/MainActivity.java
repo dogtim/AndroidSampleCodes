cccf2da46c3e0cbf6b2088ed194e579f37275429
dogtim <fatfattim@gmail.com>
2014-03-30 01:06:18 +0800

    Practice of MediaCodec. 1. http://www.bigflake.com/mediacodec/ExtractMpegFramesTest_egl14.java.txt 2. http://www.icoding.co/2013/04/android-extract-frame

---
+++ OpenGLES/src/com/example/flower/MainActivity.java
@@ -0,0 +1,58 @@
+package com.example.flower;
+
+import com.example.flower.gles.ExtractMpegFramesTest;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends Activity {
+    final String TAG = MainActivity.this.getClass().getSimpleName();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button button = (Button) this.findViewById(R.id.button1);
+        button.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                Log.i(TAG, "Generate thumbnail here !");
+                try {
+                    ExtractMpegFramesTest test = new ExtractMpegFramesTest();
+                    test.start();
+
+                } catch (Throwable e) {
+                    Log.i(TAG, "Exception: " + e.getMessage());
+                }
+            }
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+        if (id == R.id.action_settings) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+}
